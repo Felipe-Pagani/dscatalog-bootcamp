a8d46b4a9aad5aa7751bc7c27ebf1db12546f458
Felipe Pagani <feh.pagani@gmail.com>
2021-06-08 15:11:44 -0300

    data.sql Engineering

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO tb_category (name) VALUES ('E-books')
 INSERT INTO tb_category (name) VALUES ('Electronics')
 INSERT INTO tb_category (name) VALUES ('Computes')
+INSERT INTO tb_category (name) VALUES ('Engineering')
