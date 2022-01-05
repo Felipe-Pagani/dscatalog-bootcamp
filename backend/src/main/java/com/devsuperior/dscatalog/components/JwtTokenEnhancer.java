6d67a3168e4fa272039a47d58767349d2d57731a
Felipe Pagani <feh.pagani@gmail.com>
2022-01-05 13:49:27 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/components/JwtTokenEnhancer.java
+++ backend/src/main/java/com/devsuperior/dscatalog/components/JwtTokenEnhancer.java
@@ -8,10 +8,12 @@ import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
 
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.UserRepository;
 
+@Component
 public class JwtTokenEnhancer implements TokenEnhancer{
 	
 	@Autowired
@@ -22,7 +24,7 @@ public class JwtTokenEnhancer implements TokenEnhancer{
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
 		
 		User user = userRepository.findByEmail(authentication.getName());
-		Map<String, Object> map = new HashMap();
+		Map<String, Object> map = new HashMap<>();
 		map.put("userFirstName", user.getFirstName());
 		map.put("userId", user.getId());
 		
